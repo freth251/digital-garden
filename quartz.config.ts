cf03c994714d750c621a48c8ad78a7594d9203a2
freth251 <amentamerat@gmail.com>
2024-02-21 22:59:34 -0500

    Quartz sync: Feb 21, 2024, 10:59 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "የአሜን ድህረ ገጽ | Amen's website",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
