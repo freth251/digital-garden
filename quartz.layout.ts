82e81704ed6ef9ee2cc915cac0b8451046b02e3a
freth251 <amentamerat@gmail.com>
2025-03-20 06:51:11 -0400

    Quartz sync: Mar 20, 2025, 6:51 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/freth251",
+      "LinkedIn": "https://www.linkedin.com/in/amen-abshir2151/",
+      "Twitter": "https://twitter.com/a1ath251"
     },
   }),
 }
